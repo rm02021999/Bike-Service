c5d7b29a8404fda10a3bdb51387ee4d2afa3a31f
Rahul Mahato <65120414+rm02021999@users.noreply.github.com>
2021-08-15 12:39:47 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,9 +1,22 @@
 from datetime import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
-from message import automate_message
+from twilio.rest import Client 
 
 sched = BlockingScheduler()
+account_sid = 'AC5e7d9b0bdf266ba786913630b8a0d8d4' 
+auth_token = '201b9f8460882601cda85667d0427202'
+client = Client(account_sid, auth_token) 
 
-sched.add_job(automate_message, 'date', run_date='2021-09-10 10:00:00') 
+@sched.scheduled_job('cron', day_of_week='mon-sun',secoonds=55, timezone='Asia/Kolkata')
+def automate_message():
+    message = client.messages.create( 
+                                from_='whatsapp:+14155238886',  
+                                body='Good Morning Rohit, This alert message from Samol Automobile Servicing. Kindly come and have your bike service by 12/09/21 for better performance.We are happy to serve you.',      
+                                to='whatsapp:+918581803536' 
+                            ) 
+    
+    print(message.sid)()
 
 sched.start()
+
+
